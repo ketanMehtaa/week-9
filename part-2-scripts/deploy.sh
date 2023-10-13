b97ddf9f2211471fc4edc9cf5cfb34d6b096640a
Ketan Mehta <meetketanmehta@gmail.com>
2023-10-13 11:59:35 +0530

    fda

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,20 +1,8 @@
-# #!/bin/bash
-# export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
-
-# cd Week-5-with-axios-and-backend
-#  git pull origin master
-#  cd server
-#  pm2 kill
-#  pm2 start index.js
-
-
 #!/bin/bash
-# export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v16.20.2/bin:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
-
-cd week-9
-git pull origin dev
-cd server
-pm2 kill
-pm2 start index.js
+cd Week-5-with-axios-and-backend
+ git pull origin master
+ cd server
+ pm2 kill
+ pm2 start index.js
